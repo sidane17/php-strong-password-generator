29ebea3f40bd64f9a3cd13f7b0f2c4bcb4dea0d8
sidane17 <khalilsidane89@gmail.com>
2024-06-12 16:08:49 +0200

    milstone2

--- creaPassword.php
+++ creaPassword.php
@@ -1,13 +1,21 @@
 <?php
+$genera='disdbey48t23853beib833nf';
 
 $num= $_GET["num"] ?? "VUOTO";
 echo $num;
-$p='';
-function generaP($num,$p){
-    for($i=1;$i<$num;$i++){
-        $p= $p .'x';
+
+function generaP($num,$genera){
+    $n=strlen($genera);
+    $p='';
+    for($i=0;$i<$num;$i++){
+        
+        $p.=$genera[(random_int(0,($n-1)))];
+
     }
     return $p;
 }
 
-echo generaP($num,$p);
+
+//echo random_int(1,3);
+echo $genera[2];
+echo generaP($num,$genera);
